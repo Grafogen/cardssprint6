49967a3b5d5c272ab484ab343a9e9d27c9375d1d
dtolkachev <dtolkachev.04@gmail.com>
2024-02-14 00:29:53 +0300

    typography

--- cards/src/components/typography/typography.stories.ts
+++ cards/src/components/typography/typography.stories.ts
@@ -33,7 +33,91 @@ type Story = StoryObj<typeof meta>
 
 export const Large: Story = {
   args: {
-    children: 'Card content',
+    children: 'Card content xxcvcxv',
     variant: 'large',
+  },
+}
+
+export const H1: Story = {
+  args: {
+    children: 'Card content',
+    variant: 'h1',
+  },
+}
+
+export const H2: Story = {
+  args: {
+    children: 'Card content',
+    variant: 'h2',
+  },
+}
+
+export const H3: Story = {
+  args: {
+    children: 'Card content',
+    variant: 'h3',
+  },
+}
+
+export const Body1: Story = {
+  args: {
+    children: 'Card content',
+    variant: 'body1',
+  },
+}
+
+export const Body2: Story = {
+  args: {
+    children: 'Card content',
+    variant: 'body2',
+  },
+}
+
+export const Subtitle1: Story = {
+  args: {
+    children: 'Card content',
+    variant: 'subtitle1',
+  },
+}
+
+export const Subtitle2: Story = {
+  args: {
+    children: 'Card content',
+    variant: 'subtitle2',
+  },
+}
+
+export const Caption: Story = {
+  args: {
+    children: 'Card content',
+    variant: 'caption',
+  },
+}
+
+export const Overline: Story = {
+  args: {
+    children: 'Card content',
+    variant: 'overline',
+  },
+}
+
+export const Link1: Story = {
+  args: {
+    children: 'Card content',
+    variant: 'link1',
+  },
+}
+
+export const Link2: Story = {
+  args: {
+    children: 'Card content',
+    variant: 'link2',
+  },
+}
+
+export const Error: Story = {
+  args: {
+    children: 'Card content',
+    variant: 'error',
   },
 }
