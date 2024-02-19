bc67c7b98b3bbddab21e77c9b106ea499d2269e1
dtolkachev <dtolkachev.04@gmail.com>
2024-02-19 23:16:58 +0300

    input

--- cards/src/components/ui/input/input.stories.ts
+++ cards/src/components/ui/input/input.stories.ts
@@ -1,13 +1,35 @@
 import type { Meta, StoryObj } from '@storybook/react'
-import { Input } from './index'
+
+import { Input } from './'
 
 const meta = {
+  title: 'Components/Input',
   component: Input,
   tags: ['autodocs'],
-  title: 'Components/Input',
 } satisfies Meta<typeof Input>
 
 export default meta
 type Story = StoryObj<typeof meta>
 
-export const Default: Story = {}
+export const Default: Story = {
+  args: {
+    label: 'Label',
+    placeholder: 'Placeholder',
+  },
+}
+
+export const Password: Story = {
+  args: {
+    label: 'Label',
+    placeholder: 'Password',
+    type: 'password',
+  },
+}
+
+export const Error: Story = {
+  args: {
+    label: 'Input with error',
+    value: 'Wrong value',
+    errorMessage: 'Error message',
+  },
+}
--- cards/src/components/ui/input/input.tsx
+++ cards/src/components/ui/input/input.tsx
@@ -1,34 +1,94 @@
-import React, { ElementType, useState } from 'react'
+import { ComponentProps, ComponentPropsWithoutRef, forwardRef, useState } from 'react'
+
+import { clsx } from 'clsx'
+
+import { Typography } from '../../typography'
+import VisibilityOff from '@/svgs/visibility-off'
 import s from './input.module.scss'
+import Eye from '@/svgs/eye'
 
-export type InputProps<T extends ElementType = 'input'> = {
-  as?: T
-  disabled?: boolean
-  active?: boolean
-  error?: boolean
-  type?: 'input' | 'search'
-  valueInInput: string
-}
+export type TextFieldProps = {
+  onValueChange?: (value: string) => void
+  containerProps?: ComponentProps<'div'>
+  labelProps?: ComponentProps<'label'>
+  errorMessage?: string
+  label?: string
+} & ComponentPropsWithoutRef<'input'>
 
-export const Input = <T extends ElementType = 'input'>(props: InputProps<T>) => {
-  const {
-    as: Component = 'input',
-    valueInInput = 'Input',
-    disabled,
-    active,
-    type = 'input',
-    error,
-  } = props
+export const Input = forwardRef<HTMLInputElement, TextFieldProps>(
+  (
+    {
+      className,
+      errorMessage,
+      placeholder,
+      type,
+      containerProps,
+      labelProps,
+      label,
+      onChange,
+      onValueChange,
+      ...restProps
+    },
+    ref
+  ) => {
+    const [showPassword, setShowPassword] = useState(false)
 
-  const [inputValue, setInputValue] = useState(valueInInput)
+    const isShowPasswordButtonShown = type === 'password'
 
-  const onClickValueClean = () => {
-    setInputValue('')
+    const finalType = getFinalType(type, showPassword)
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+      onChange?.(e)
+      onValueChange?.(e.target.value)
+    }
+
+    const classNames = {
+      root: clsx(s.root, containerProps?.className),
+      fieldContainer: clsx(s.fieldContainer),
+      field: clsx(s.field, !!errorMessage && s.error, className),
+      label: clsx(s.label, labelProps?.className),
+      error: clsx(s.error),
+    }
+
+    return (
+      <div className={classNames.root}>
+        {label && (
+          <Typography variant="body2" as="label" className={classNames.label}>
+            {label}
+          </Typography>
+        )}
+        <div className={classNames.fieldContainer}>
+          <input
+            className={classNames.field}
+            placeholder={placeholder}
+            ref={ref}
+            type={finalType}
+            onChange={handleChange}
+            {...restProps}
+          />
+          {isShowPasswordButtonShown && (
+            <button
+              className={s.showPassword}
+              type={'button'}
+              onClick={() => setShowPassword(prev => !prev)}
+            >
+              {showPassword ? <VisibilityOff /> : <Eye />}
+            </button>
+          )}
+        </div>
+
+        <Typography variant="error" className={classNames.error}>
+          {errorMessage}
+        </Typography>
+      </div>
+    )
+  }
+)
+
+function getFinalType(type: ComponentProps<'input'>['type'], showPassword: boolean) {
+  if (type === 'password' && showPassword) {
+    return 'text'
   }
 
-  return (
-    <div>
-      <Component className={s.root} value={inputValue} onClick={onClickValueClean} />
-    </div>
-  )
+  return type
 }
